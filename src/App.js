317b964730918ec3acc294dfcf8004a8e535e369
pskanade <k.pranav.apk@gmail.com>
2018-11-20 02:54:03 +0530

    Dynamic UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h3>This is my first react application</h3>
+        <Person />
       </div>
     );
   }
