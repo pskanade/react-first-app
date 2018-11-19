0b6c29fdfcde5542bae89c5a64da22bce1cd76f3
pskanade <k.pranav.apk@gmail.com>
2018-11-20 02:33:27 +0530

    The best practice

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h3>This is my first react application</h3>
-    //   </div>
-    // );
-
-    // return React.createElement("div", null, "h1", "This"); here the h1 tag // won't get applied
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h3", null, "This is heading!")
+    return (
+      <div className="App">
+        <h3>This is my first react application</h3>
+      </div>
     );
   }
 }
