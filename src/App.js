84e3833d5244dc4c9e47441cb9df43640965f9d8
pskanade <k.pranav.apk@gmail.com>
2018-11-20 02:29:14 +0530

    First tric to get the app working with just a heading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h3>This is my first react application</h3>
+    //   </div>
+    // );
+
+    // return React.createElement("div", null, "h1", "This"); here the h1 tag // won't get applied
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h3", null, "This is heading!")
     );
   }
 }
